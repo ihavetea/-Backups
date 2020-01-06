360c331414bcf07489c0a322fcbb5402b45f5bef
ihavetea <ihavetea@gmail.com>
2020-01-06 04:48:11 +0100

    autopush

--- installscript.sh
+++ installscript.sh
@@ -30,7 +30,7 @@
 #   set;defaultCommand;dg
 
 # Compose Key /////////////////////////////////////////////////////////////////////////////
-setxkbmap -option compose:caps
+setxkbmap -option compose:prsc
 
 # WIFI ####################################################################################
 sudo pacman -Syu
@@ -49,9 +49,9 @@ sudo pacman -S alsa-utils
 sudo pacman -S fzf
 sudo pacman -S ranger
 sudo pacman -S cmus #:colorscheme green mono 88
-sudo pacman -S inkscape
-sudo pacman -S krita
-sudo pacman -S gimp
+#sudo pacman -S inkscape
+#sudo pacman -S krita
+#sudo pacman -S gimp
 sudo pacman -S calcurse #calendar
 sudo pacman -S mps-youtube
 sudo pacman -S youtube-dl
@@ -92,16 +92,13 @@ pulseaudio-start
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # VIM #####################################################################################
+
 # Gruvbox & Airline  //////////////////////////////////////////////////////////////////////
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 
-
-# DROPBOX #################################################################################
-cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
-~/.dropbox-dist/dropboxd
 # Vim Notes ///////////////////////////////////////////////////////////////////////////////
 cd ~/.vim/bundle/
 wget http://peterodding.com/code/vim/downloads/notes.zip
@@ -109,9 +106,11 @@ wget http://peterodding.com/code/vim/downloads/misc.zip
 
 unzip ./notes.zip -d vim-notes
 unzip ./misc.zip -d vim-misc
+rm ./notes.zip && rm ./misc.zip
 
-rm ./notes.zip
-rm ./misc.zip
+# DROPBOX #################################################################################
+#cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+#~/.dropbox-dist/dropboxd
 
 # HUNDRED RABBITS #########################################################################
 cd ~/bin
@@ -128,37 +127,26 @@ git clone https://github.com/hundredrabbits/Poodle.git
 git clone https://github.com/hundredrabbits/Noodle.git
 
 # VCV RACK ################################################################################
-wget https://vcvrack.com/downloads/Rack-1.1.6-lin.zip
-unzip Rack-1.1.6-lin.zip
-rm Rack-1.1.6-lin.zip
+#wget https://vcvrack.com/downloads/Rack-1.1.6-lin.zip
+#unzip Rack-1.1.6-lin.zip && rm Rack-1.1.6-lin.zip
 
 # REAPER ##################################################################################
 #wget https://www.reaper.fm/files/6.x/reaper602_linux_x86_64.tar.xz
 
 # SUCKLESS ################################################################################
 
-cd
-git clone https://github.com/ihavetea/-suckless.git
-mv ~/-suckless ~/Suckless
-cd ~/Suckless
+cd && git clone https://github.com/ihavetea/-suckless.git
+mv ~/-suckless ~/Suckless && cd ~/Suckless
 
-cd dwm
-sudo make clean install
+cd dwm && sudo make clean install
+cd ../st && sudo make clean install
+cd ../dwmblocks && sudo make clean install
+cd ../dmenu && sudo make clean install
+cd ../surf && sudo make clean install
+cd ../slock && sudo make clean install
 
-cd ../st
-sudo make clean install
-
-cd ../dwmblocks
-sudo make clean install
-
-cd ../dmenu
-sudo make clean install
-
-cd ../surf
-sudo make clean install
-
-cd ../slock
-sudo make clean install
+# WALLPAPERS ##############################################################################
+#cd && git clone https://github.com/LukeSmithxyz/wallpapers
 
 # RC FILES + BACKGROUND ###################################################################
 #cd
